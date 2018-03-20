d4cfc742d52e2ea2a54fee1e0cd903f41418d7ad
정경환 <sujkh@nomadconnection.com>
2018-03-20 23:38:52 +0900

    bud fix

--- dist/react-short-notice.js
+++ dist/react-short-notice.js
@@ -1,17 +1,219 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(["module", "src/ShortNoticeContainer", "src/ShortNoticeContraller"], factory);
+    define(['exports', 'react'], factory);
   } else if (typeof exports !== "undefined") {
-    factory(module, require("src/ShortNoticeContainer"), require("src/ShortNoticeContraller"));
+    factory(exports, require('react'));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod, global.ShortNoticeContainer, global.ShortNoticeContraller);
+    factory(mod.exports, global.react);
     global.index = mod.exports;
   }
-})(this, function (module, ShortNoticeContainer, ShortNoticeContraller) {
-  "use strict";
+})(this, function (exports, _react) {
+  'use strict';
 
-  module.exports = { ShortNoticeContainer: ShortNoticeContainer, ShortNoticeContraller: ShortNoticeContraller };
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.ShortNoticeContraller = exports.ShortNoticeContainer = undefined;
+
+  var _react2 = _interopRequireDefault(_react);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var _createClass = function () {
+    function defineProperties(target, props) {
+      for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
+      }
+    }
+
+    return function (Constructor, protoProps, staticProps) {
+      if (protoProps) defineProperties(Constructor.prototype, protoProps);
+      if (staticProps) defineProperties(Constructor, staticProps);
+      return Constructor;
+    };
+  }();
+
+  function _possibleConstructorReturn(self, call) {
+    if (!self) {
+      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    }
+
+    return call && (typeof call === "object" || typeof call === "function") ? call : self;
+  }
+
+  function _inherits(subClass, superClass) {
+    if (typeof superClass !== "function" && superClass !== null) {
+      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+    }
+
+    subClass.prototype = Object.create(superClass && superClass.prototype, {
+      constructor: {
+        value: subClass,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+  }
+
+  var ShortNoticeContainer = exports.ShortNoticeContainer = function (_Component) {
+    _inherits(ShortNoticeContainer, _Component);
+
+    function ShortNoticeContainer(props) {
+      _classCallCheck(this, ShortNoticeContainer);
+
+      var _this = _possibleConstructorReturn(this, (ShortNoticeContainer.__proto__ || Object.getPrototypeOf(ShortNoticeContainer)).call(this, props));
+
+      _this.state = {
+        isShow: false,
+        id: _this.props.id ? _this.props.id : 'empty'
+      };
+
+      _this.timer = null;
+      _this.onListener = _this.onListener.bind(_this);
+      _this.show = _this.show.bind(_this);
+      _this.hide = _this.hide.bind(_this);
+      return _this;
+    }
+
+    _createClass(ShortNoticeContainer, [{
+      key: 'onListener',
+      value: function onListener(e) {
+        var type = e.detail.type;
+
+
+        switch (type) {
+          case 'show':
+            this.show(e.detail.payload);
+            break;
+          case 'hide':
+            this.hide(e.detail.payload);
+            break;
+          default:
+            break;
+        }
+      }
+    }, {
+      key: 'show',
+      value: function show(payload) {
+        var _this2 = this;
+
+        var id = this.state.id;
+        var timeout = this.props.timeout;
+
+        if (id === payload.id) {
+          this.setState({
+            isShow: true
+          }, function () {
+            if (timeout) {
+              setTimeout(function () {
+                _this2.setState({
+                  isShow: false
+                });
+              }, timeout * 1);
+            }
+          });
+        }
+      }
+    }, {
+      key: 'hide',
+      value: function hide(payload) {
+        var id = this.state.id;
+
+        if (id === payload.id) {
+          this.setState({
+            isShow: false
+          });
+        }
+      }
+    }, {
+      key: 'componentWillMount',
+      value: function componentWillMount() {
+        window.addEventListener('short-notice', this.onListener, false);
+      }
+    }, {
+      key: 'componentWillUnmount',
+      value: function componentWillUnmount() {
+        if (this.timer) {
+          clearTimeout(this.timer);
+          timer = null;
+        }
+        window.removeEventListener('short-notice', this.onListener, false);
+      }
+    }, {
+      key: 'render',
+      value: function render() {
+        var isShow = this.state.isShow;
+        var transition = this.props.transition;
+
+        if (transition) {
+          return _react2.default.createElement(
+            'div',
+            { style: { transition: 'all ' + transition, opacity: isShow ? 1 : 0 } },
+            this.props.children
+          );
+        } else {
+          return isShow ? this.props.children : null;
+        }
+      }
+    }]);
+
+    return ShortNoticeContainer;
+  }(_react.Component);
+
+  var ShortNoticeContraller = exports.ShortNoticeContraller = function () {
+    function ShortNoticeContraller() {
+      _classCallCheck(this, ShortNoticeContraller);
+    }
+
+    _createClass(ShortNoticeContraller, null, [{
+      key: 'show',
+      value: function show(id) {
+        var event = new CustomEvent('short-notice', {
+          detail: {
+            type: 'show',
+            payload: {
+              id: id
+            }
+          }
+        });
+        window.dispatchEvent(event);
+      }
+    }, {
+      key: 'hide',
+      value: function hide(id) {
+        var event = new CustomEvent('short-notice', {
+          detail: {
+            type: 'hide',
+            payload: {
+              id: id
+            }
+          }
+        });
+        window.dispatchEvent(event);
+      }
+    }]);
+
+    return ShortNoticeContraller;
+  }();
+
+  exports.default = { ShortNoticeContainer: ShortNoticeContainer, ShortNoticeContraller: ShortNoticeContraller };
 });
--- index.js
+++ index.js
@@ -1,6 +1,116 @@
-"use strict";
+import React, {Component} from 'react';
 
-var ShortNoticeContainer = require("src/ShortNoticeContainer")
-var ShortNoticeContraller = require("src/ShortNoticeContraller")
+export class ShortNoticeContainer extends Component {
+  constructor(props){
+    super(props)
 
-module.exports = {ShortNoticeContainer, ShortNoticeContraller}
+    this.state = {
+      isShow:false,
+      id:this.props.id ? this.props.id : 'empty'
+    }
+
+    this.timer = null
+    this.onListener = this.onListener.bind(this)
+    this.show = this.show.bind(this)
+    this.hide = this.hide.bind(this) 
+  }
+
+  onListener(e){
+    const {type} = e.detail;
+    
+    switch (type) {
+      case 'show':
+        this.show(e.detail.payload)
+        break;
+      case 'hide':
+        this.hide(e.detail.payload);
+        break;
+      default:
+        break;
+    }
+  }
+
+  show(payload){
+    const {id} = this.state
+    const {timeout} = this.props
+    if(id === payload.id){
+      this.setState({
+        isShow:true
+      },()=>{
+        if(timeout){
+          setTimeout(() => {
+            this.setState({
+              isShow:false
+            })
+          }, timeout*1);
+        }
+      })
+    }
+  }
+
+  hide(payload){
+    const {id} = this.state
+    if(id === payload.id){
+      this.setState({
+        isShow:false
+      })
+    }
+  }
+
+  componentWillMount() {
+    window.addEventListener('short-notice',this.onListener, false);
+  }
+
+  componentWillUnmount() {
+    if(this.timer){
+      clearTimeout(this.timer)
+      timer = null
+    }
+    window.removeEventListener('short-notice',this.onListener, false);
+  }
+
+  render(){
+    const {isShow} = this.state
+    const {transition} = this.props
+    if(transition){
+      return(
+        <div style={{transition:`all ${transition}`,opacity:isShow?1:0}}>
+          {this.props.children}
+        </div>
+      )
+    }
+    else {
+      return isShow ? this.props.children : null
+    }
+  }
+}
+
+
+export class ShortNoticeContraller {
+  static show(id){
+    let event = new CustomEvent('short-notice', {
+      detail: {
+        type:'show',
+        payload:{
+          id:id
+        }
+      }
+    });
+    window.dispatchEvent(event);
+  }
+
+  static hide(id){
+    let event = new CustomEvent('short-notice', {
+      detail: {
+        type:'hide',
+        payload:{
+          id:id
+        }
+      }
+    });
+    window.dispatchEvent(event);
+  }
+}
+
+
+export default {ShortNoticeContainer, ShortNoticeContraller}
